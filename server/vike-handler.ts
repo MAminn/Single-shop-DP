baefb5d5ba8a940b1b2f5be73c775a3ea6fa920f
OmarMAttia7 <omarmattia97@gmail.com>
2025-03-22 16:38:37 +0200

    refactor: remove unused code

--- server/vike-handler.ts
+++ server/vike-handler.ts
@@ -1,12 +1,9 @@
 /// <reference lib="webworker" />
 import { renderPage } from "vike/server";
-// TODO: stop using universal-middleware and directly integrate server middlewares instead. (Bati generates boilerplates that use universal-middleware https://github.com/magne4000/universal-middleware to make Bati's internal logic easier. This is temporary and will be removed soon.)
-import type { Get, UniversalHandler } from "@universal-middleware/core";
 import { createMiddleware } from "hono/factory";
 
 export const vikeHonoMiddleware = createMiddleware<HonoContext.Env>(
   async (c) => {
-    // console.log(c.var.clientSession)
     const pageContextInit = {
       db: c.var.db,
       clientSession: c.var.clientSession,
