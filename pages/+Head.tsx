68f59a384be2a8ebd321742e38bc13f05002c129
seif ahmed <saif.ahmed1215@gmail.com>
2025-05-15 19:00:01 +0300

    analytics trial 2

--- pages/+Head.tsx
+++ pages/+Head.tsx
@@ -19,28 +19,27 @@ export default function HeadDefault() {
   }, []);
 
   // Initialize Google Analytics
-  useEffect(() => {
-    // Load Google Analytics script
-    const script = document.createElement("script");
-    script.src = "https://www.googletagmanager.com/gtag/js?id=G-4P688CTR6M";
-    script.async = true;
-    document.head.appendChild(script);
+useEffect(() => {
+  const script = document.createElement("script");
+  script.src = "https://www.googletagmanager.com/gtag/js?id=G-4P688CTR6M";
+  script.async = true;
 
-    // Initialize dataLayer and gtag function
+  // When script loads, init GA
+  script.onload = () => {
     window.dataLayer = window.dataLayer || [];
     window.gtag = (...args) => {
       window.dataLayer.push(args);
     };
     window.gtag("js", new Date());
     window.gtag("config", "G-4P688CTR6M");
+  };
 
-    // Cleanup on unmount
-    return () => {
-      if (script.parentNode) {
-        script.parentNode.removeChild(script);
-      }
-    };
-  }, []);
+  document.head.appendChild(script);
+
+  return () => {
+    script.remove();
+  };
+}, []);
 
   return (
     <>
--- pages/+onPageTransitionStart.ts
+++ pages/+onPageTransitionStart.ts
@@ -2,4 +2,9 @@ import type { OnPageTransitionStartAsync } from "vike/types";
 
 export const onPageTransitionStart: OnPageTransitionStartAsync = async () => {
   document.querySelector("body")?.classList.add("page-is-transitioning");
+  if (typeof window.gtag === "function") {
+    window.gtag("event", "page_view", {
+      page_path: window.location.pathname + window.location.search,
+    });
+  }
 };
